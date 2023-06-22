9f440f701d9fd40bff9bce91325a9a1795d24dfe
kimseunghyun-kr <incongnito12@gmail.com>
2023-06-23 02:11:56 +0900

    initial commit

---
+++ src/main/java/hello/login/domain/member/Member.java
@@ -0,0 +1,16 @@
+package hello.login.domain.member;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
+public class Member {
+
+    private Long id;
+    @NotEmpty
+    private String loginId; //로그인 ID
+    @NotEmpty
+    private String name; //사용자 이름
+    @NotEmpty
+    private String password;
+}
