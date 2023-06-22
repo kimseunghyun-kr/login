739f0d826ebe569952941a5c5f8a0be7d8db04fa
kimseunghyun-kr <incongnito12@gmail.com>
2023-06-23 01:52:53 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.1.0"
+	id("io.spring.dependency-management") version "1.1.0"
+}
+
+group = "hello"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation ("org.openjdk.nashorn:nashorn-core:15.3")
+//	implementation("org.graalvm.js:js:21.2.0")
+//	implementation("org.graalvm.sdk:graal-sdk:22.0.0.2")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
